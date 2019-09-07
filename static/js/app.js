de51ece30bcc69e7081f65f906de558abb3a834e
faznaimov <faz.naimov@gmail.com>
2019-09-07 19:34:42 -0400

    added gauge to the dashboard

--- static/js/app.js
+++ static/js/app.js
@@ -22,6 +22,67 @@ function buildMetadata(sample) {
     
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+    // Enter a speed between 0 and 180
+    var level = sample.WFREQ;
+
+    // Trig to calc meter point
+    var degrees = 180 - (level*20),
+         radius = .7;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+         pathX = String(x),
+         space = ' ',
+         pathY = String(y),
+         pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    
+    var data = [{ type: 'scatter',
+       x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: "times per week",
+        text: level,
+        hoverinfo: 'text+name'},
+      { values: [45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 50],
+      rotation: 90,
+      text: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3',
+                '1-2', '0-1', ''],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['#84B589','rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                             'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                             'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                             '#F4F1E4','#F8F3EC', 'rgba(255, 255, 255, 0)',]},
+      labels: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3',
+      '1-2', '0-1', ''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+    
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+
+      title: 'Belly Button Wash Frequency',
+      text: 'Scrubs per Week',
+      xaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]}
+    };
+    Plotly.newPlot('gauge', data, layout);
   }
 )};
 
@@ -57,25 +118,23 @@ function buildCharts(sample) {
     };
 
     Plotly.newPlot('bubble', data, layout);
-   
+    })
 
-    // @TODO: Build a Pie Chart
-    d3.json(url).then(function(data) {  
-    var pie_values = data.sample_values.slice(0,10);
-      var pie_labels = data.otu_ids.slice(0,10);
-      var pie_hover = data.otu_labels.slice(0,10);
+  // @TODO: Build a Pie Chart
+  d3.json(url).then(function(data) {  
+  var pie_values = data.sample_values.slice(0,10);
+    var pie_labels = data.otu_ids.slice(0,10);
+    var pie_hover = data.otu_labels.slice(0,10);
 
-      var data = [{
-        values: pie_values,
-        labels: pie_labels,
-        hovertext: pie_hover,
-        type: 'pie'
-      }];
+    var data = [{
+      values: pie_values,
+      labels: pie_labels,
+      hovertext: pie_hover,
+      type: 'pie'
+    }];
 
-      Plotly.newPlot('pie', data);
-
-    });
-  });   
+    Plotly.newPlot('pie', data);
+  });
 }
 
 function init() {
